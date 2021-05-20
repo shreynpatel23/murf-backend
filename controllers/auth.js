64c17bc5d191d9b486c77b4a06f92a7c1c9be049
Shrey Patel <shreynpatel23@gmail.com>
2021-05-20 11:50:30 +0530

    added flow if the user is already present

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,32 +3,13 @@ const jwt = require("jsonwebtoken");
 
 exports.register = async (request, response) => {
   // check whether email is present in our database or not.
-  const emailExist = await User.findOne({ email: request.body.email });
-  if (emailExist) return response.status(400).send("email already exist");
-  // const user = await User.findOne({ email: request.body.email });
-  // const token = jwt.sign({ id: request.body._id }, process.env.TOKEN_SECRET);
-  // if (user) {
-  //   try {
-  //     const { _doc } = user;
-  //     return (
-  //       response.header("authToken", token),
-  //       response.json({
-  //         id: _doc._id,
-  //         name: _doc.name,
-  //         email: _doc.email,
-  //         imageUrl: _doc.imageUrl,
-  //         forumId: _doc.forumId,
-  //         token: token,
-  //       })
-  //     );
-  //     // return response
-  //     // .status(400)
-  //     // .send({ error: "email already exist", data: user });
-  //   } catch (err) {
-  //     response.status(400).json("please provide valid data point");
-  //   }
-  // }
-
+  const token = jwt.sign({ id: request.body._id }, process.env.TOKEN_SECRET);
+  const user = await User.findOne({ email: request.body.email });
+  if (user)
+    return response.status(200).send({
+      error: "email already exist",
+      data: { ...user._doc, token: token },
+    });
   try {
     const newUser = new User({
       name: request.body.name,
@@ -40,11 +21,14 @@ exports.register = async (request, response) => {
       .then((res) => {
         response.header("authToken", token);
         response.json({
-          id: res._id,
-          token: token,
-          name: res.name,
-          email: res.email,
-          imageUrl: res.imageUrl,
+          error: "",
+          data: {
+            _id: res._id,
+            token: token,
+            name: res.name,
+            email: res.email,
+            imageUrl: res.imageUrl,
+          },
         });
       })
       .catch((err) => {
